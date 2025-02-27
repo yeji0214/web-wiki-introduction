04d1d006e703a54927078792f9cb8a24cdd082f8
yeji0214 <1225yj50@naver.com>
2025-02-27 14:16:31 +0900

    feat: 댓글 입력창 구현

--- js/index.js
+++ js/index.js
@@ -17,3 +17,28 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+
+const registerComment = () => {
+  const inputComment = document.getElementById('input-comment');
+  const commentList = document.getElementById('comment-list')
+  if (!inputComment.value.trim()) {
+    alert('댓글을 입력하셔야죠..');
+    return;
+  }
+
+  const newComment = document.createElement('li');
+
+  newComment.innerHTML = `
+    <div id="comment-item">
+      <div id="comment-author">
+        <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+        <span>방문자</span>
+      </div>
+      <div id="comment-content">${inputComment.value}</div>
+    </div>
+  `;
+
+  commentList.appendChild(newComment);
+
+  inputComment.value = '';
+};
